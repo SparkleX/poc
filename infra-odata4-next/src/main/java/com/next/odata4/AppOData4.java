1027aa849d91676afd9a6587df008705788faa34
NeXT <sunxufei@hotmail.com>
2019-01-23 21:07:43 +0800

    add

--- infra-odata4-next/src/main/java/com/next/odata4/AppOData4.java
+++ infra-odata4-next/src/main/java/com/next/odata4/AppOData4.java
@@ -1,23 +1,39 @@
 package com.next.odata4;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.next.odata4.config.ODataServiceScan;
+import com.querydsl.core.types.Predicate;
 
+import gen.table.BmoOCRD;
 import gen.table.BmoORDR;
 
 @SpringBootApplication
-@ServletComponentScan
+//@ServletComponentScan
 @EntityScan(basePackageClasses = BmoORDR.class)
-@ODataServiceScan
+//@ODataServiceScan
+@RestController
 public class AppOData4
 {
-
+	@Autowired
+	RepoOrder repoOrder;
+	@RequestMapping(method = RequestMethod.GET, value = "/users")
+	@ResponseBody
+	public Iterable<BmoORDR> findAllByWebQuerydsl(
+	  @QuerydslPredicate(root = BmoORDR.class) Predicate predicate) {
+	    return repoOrder.findAll(predicate);
+	}
 	public static void main(String[] args)
 	{
 		ApplicationContext appCtx = SpringApplication.run(AppOData4.class, args);
--- infra-odata4-next/src/main/java/com/next/odata4/RepoOrder.java
+++ infra-odata4-next/src/main/java/com/next/odata4/RepoOrder.java
@@ -2,13 +2,18 @@ package com.next.odata4;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.repository.CrudRepository;
 
 import gen.table.BmoORDR;
 
-public interface RepoOrder extends CrudRepository<BmoORDR, Integer> 
+public interface RepoOrder extends JpaRepository<BmoORDR, Integer>, QuerydslPredicateExecutor<BmoORDR>
 {
+
+
 	@Query("select t from BmoORDR t where t.id = ?1")
 	List<BmoORDR> findByMyId(Integer integer);
 }
