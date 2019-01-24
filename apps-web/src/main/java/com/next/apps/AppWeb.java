f8d39ec66690865c9eed0cfb91e7f92fefca364c
NeXT <sunxufei@hotmail.com>
2019-01-24 19:49:06 +0800

    add

--- apps-web/src/main/java/com/next/apps/AppWeb.java
+++ apps-web/src/main/java/com/next/apps/AppWeb.java
@@ -3,9 +3,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import gen.table.BmoORDR;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = BmoORDR.class)
+@EnableSwagger2
 public class AppWeb
 {
 	public static void main(String[] args)
